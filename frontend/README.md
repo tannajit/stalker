d670bca3f0e321ec5ac377bf1d3bad3414af9002
Fadma ENNAJI <f.ennaji@fgddistrib.local>
2021-11-24 14:41:39 +0100

    initial commit

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# Staker
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.4.
 
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'staker'`, () => {
+  it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('staker');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('staker app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frontend app is running!');
   });
 });
