cf484a29a695921b4388f00262bd2bd562d477a1
Fadma ENNAJI <f.ennaji@fgddistrib.local>
2021-11-29 08:55:38 +0100

    map display

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,15 +8,10 @@ import { AuthenticationService } from './authentication.service';
 })
 export class AppComponent {
   title = 'frontend';
-<<<<<<< HEAD
-  constructor(private _auth:AuthenticationService){}
-  getLoged(){
-=======
   constructor(private _auth:AuthenticationService){
   
   }
   getLogged(){
->>>>>>> origin/master
     return this._auth.loggedIn()
   }
 }
