a68ccd4281c7b872058f3e9052bb94df99444a10
Hajar OUAZIZ <h.ouaziz@fgddistrib.local>
2022-01-17 15:16:55 +0100

    hhhmmmmmmmm

--- frontend/src/app/admin/update-user/update-user.component.ts
+++ frontend/src/app/admin/update-user/update-user.component.ts
@@ -72,11 +72,10 @@ export class UpdateUserComponent implements OnInit {
 
     // }
 
-    console.log("userInfoSectors",this.userInfo.sectors)
 
     this.userInfo.sectors.forEach(el => {this.SelectedSector.push(""+el.nameSecteur)});
     this.SectorsAttached=this.SelectedSector
-    
+    console.log("SelectedSector",this.SelectedSector)
     this._setting.getSettings('param=role').subscribe(res => {
       this.Roles = res.details.roles
       console.log(res)
